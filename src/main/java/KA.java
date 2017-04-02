caa6a2dc6d397fc78b84724c0a7dd5bb75583f6d
vavi-vavi <vavilon951@gmail.com>
2017-04-03 00:48:36 +0300

    Testing

--- src/main/java/KA.java
+++ src/main/java/KA.java
@@ -1,20 +1,22 @@
+import java.util.ArrayList;
+
 import static java.lang.Math.*;
 
 /**
  * Created by Vavi on 02.04.2017.
  */
 public class KA {
-    public static double Rysl = 6356.766; //Условный радиус Земли
+    public static double Rysl = 6356.766; //Условный радиус Земли метры
     public static double g0 = 9.80665;//ускор своб пад
     public static double R = 287.05287;//Газ пост
     public static double g0R = 0.0341632188;
-    public static double Rekv = 6378.1; //Экваториальный радиус Земли
-    public static double fcg = 1/298.258;//Сжатие Земли
+    public static double Rekv = 6378.1; //Экваториальный радиус Земли метры
+    public static double fcg = 0.003352824419;//Сжатие Земли
     public static double u = 398600;//грав пост
     public static double cx = 1.3;//коэф силы лоб сопр
     public static double Smid = 15; //площадь миделя
     public static double m = 8000; // масса КА
-    public static double w = 7.292115855;//Угловая скорость вращ Земли
+    public static double w = 7.292115855*Math.pow(10,-5);//Угловая скорость вращ Земли
     public static double K = 0.3; // Аэродинамическое качество
     //5 вариант
     public static double x = 4206.138;
@@ -23,7 +25,7 @@ public class KA {
     public static double Vx = -5.918745;
     public static double Vy = 3.008711;
     public static double Vz = 3.746175;
-    public static double yvx = -1.5;// Угол входа
+    public static double yvx = 45;// Угол входа
     //Таблица для плотности
     public static double[][] Plot =
             {{0,288.15,-0.0065,1.24915236*Math.pow(10,-1)},
@@ -42,14 +44,29 @@ public class KA {
     public static double am = 0.0;//Градниент температуры
     public static double Tm = 0.0;//абсолют температура
     public static double Fniz = 0.0;//Нижняя граница высоты
+    public static double F = 0.0;//Высота КА
     public static void main(String args[]){
-        double h=1;
-        double a = 5;
-        double b = 100;
-        Double n = (b-a)/h;
-        double[] X = new double[n.intValue()+1];
-        double[] Y = new double[n.intValue()+1];
-        int j=0;
+//        for(int i=0;i<Plot.length;i++){
+//            Plot[i][0] = Plot[i][0]*1000;
+//        }
+        double h=0.2;
+        double time0 = 0;
+        double visota0 = 100;
+
+        ArrayList<Double> time = new ArrayList<Double>();
+        ArrayList<Double> Visota = new ArrayList<Double>();
+
+        time.add(time0);
+        Visota.add(visota0);
+        double[] vect = {x,y,z,Vx,Vy,Vz};
+        while(visota0>5){
+            vect =SumVec(vect,UmChis(Fun(vect),h));
+            visota0 =F;
+            Visota.add(visota0);
+            time0 = time0+0.2;
+            time.add(time0);
+        }
+
 //        for(int i=100;i>=5;i--){
 //            X[j] = i;
 //
@@ -64,6 +81,20 @@ public class KA {
 //            System.out.print(Y[i]+" ");
 //        }
         System.out.println("Hello");
+    }
+    public static double[] UmChis(double[] Vect, double chis){
+        double[] Exit = new double[Vect.length];
+        for(int i=0;i<Vect.length;i++){
+            Exit[i] = Vect[i]*chis;
+        }
+        return Exit;
+    }
+    public static double[] SumVec(double[] A, double[] B){
+        double[] Exit = new double[A.length];
+        for(int i=0;i<A.length;i++){
+            Exit[i] = A[i]+B[i];
+        }
+        return Exit;
     }
     public static double[] Fun(double[] vect){
         double[] Exit = new double[vect.length];
@@ -98,16 +129,17 @@ public class KA {
         Exit[0] = Vx;
         Exit[1] = Vy;
         Exit[2] = Vz;
-        Exit[3] = (-u/Math.pow(r,3))*x-cx*(Smid/(2*m))*PO(xyz)*V*Vx+
-                K*cx*(Smid/(2*m))*PO(xyz)*Math.pow(V,2)*(m1*Math.cos(yvx*0.0174533)+l1*Math.sin(yvx*0.0174533))
-                +x*Math.pow(w,2)+2*w*Vy;
+        double P = PO(xyz);
+        Exit[3] =((-u/Math.pow(r,3))*x-cx*(Smid/(2*m))*P*V*Vx+
+                K*cx*(Smid/(2*m))*P*Math.pow(V,2)*(m1*Math.cos(yvx*0.0174533)+l1*Math.sin(yvx*0.0174533))
+                +x*Math.pow(w,2)+2*w*Vy);
 
-        Exit[4] = (-u/Math.pow(r,3))*y-cx*(Smid/(2*m))*PO(xyz)*V*Vy+
+        Exit[4] = ((-u/Math.pow(r,3))*y-cx*(Smid/(2*m))*PO(xyz)*V*Vy+
                 K*cx*(Smid/(2*m))*PO(xyz)*Math.pow(V,2)*(m2*Math.cos(yvx*0.0174533)+l2*Math.sin(yvx*0.0174533))
-                +y*Math.pow(w,2)-2*w*Vx;
+                +y*Math.pow(w,2)-2*w*Vx);
 
-        Exit[5] = (-u/Math.pow(r,3))*z-cx*(Smid/(2*m))*PO(xyz)*V*Vz+
-                K*cx*(Smid/(2*m))*PO(xyz)*Math.pow(V,2)*(m3*Math.cos(yvx*0.0174533)+l3*Math.sin(yvx*0.0174533));
+        Exit[5] =( (-u/Math.pow(r,3))*z-cx*(Smid/(2*m))*PO(xyz)*V*Vz+
+                K*cx*(Smid/(2*m))*PO(xyz)*Math.pow(V,2)*(m3*Math.cos(yvx*0.0174533)+l3*Math.sin(yvx*0.0174533)));
         return Exit;
     }
     public static double PO(double[] xyz){//double[] xyz
@@ -116,7 +148,7 @@ public class KA {
         double z = xyz[2];
         double h = Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2))-
                 Rekv*(1-(fcg*Math.pow(z,2))/(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2)));
-        double F = h*Rysl/(h+Rysl);
+        F = h*Rysl/(h+Rysl);
 
         Tab(F);
         double T = Tm + am*(F-Fniz);
