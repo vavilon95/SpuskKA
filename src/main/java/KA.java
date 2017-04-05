f6b03a7c18f9f71c3a02fc38f6d51f8f58fa0e21
vavilon95 <vavilon951@gmail.com>
2017-04-05 10:45:02 +0300

    jjj

--- src/main/java/KA.java
+++ src/main/java/KA.java
@@ -221,10 +221,10 @@ public class KA {
     public static void Tab(double F){
         for(int i=0;i<Plot.length-1;i++){
             if((F>=Plot[i][0])&(F<=Plot[i+1][0])){
-                Fniz = Plot[i][0];
-                p = Plot[i][0]*g0;//Плотность нижней границы
-                am = Plot[i][2];//Градниент температуры
-                Tm = Plot[i][1];//абсолют температура
+                Fniz = Plot[i+1][0];
+                p = Plot[i+1][0]*g0;//Плотность нижней границы
+                am = Plot[i+1][2];//Градниент температуры
+                Tm = Plot[i+1][1];//абсолют температура
                 break;
             }
         }
